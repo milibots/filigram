1101050f237003faaf9eed58e58a9877bb9a601a
MiliScripts <milaadfarzian@gmail.com>
2026-09-17 06:46:05 +0330

    build: add ABI splits for arm64-v8a, armeabi-v7a, x86, and x86_64

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,15 @@ android {
         vectorDrawables.useSupportLibrary = true
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     buildTypes {
         debug {
             applicationIdSuffix = ".debug"
